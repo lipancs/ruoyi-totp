e0d73394638e6a2e4466774bb232f8cfdd5ba37e
lipancs <lipancs@gmail.com>
2024-01-11 19:21:39 +0800

    补充信息

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-123456
 # ruoyi-totp
 集成totp功能
+数据库用户表中新增tcode字段，varchar类型
+在SysLoginService中扩展了validateTcode(username, tcode)验证方法
+在SysProfileController中扩展了相应重置，生成，校验等基本功能
+
--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysLoginService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysLoginService.java
@@ -71,7 +71,7 @@ public class SysLoginService
     public String login(String username, String password, String code,String tcode, String uuid)
     {
         // 验证码校验
-     //   validateCaptcha(username, code, uuid);
+        validateCaptcha(username, code, uuid);
         // 登录前置校验
         loginPreCheck(username, password);
         //增加双因子校验码校验
